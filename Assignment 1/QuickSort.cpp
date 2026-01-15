efd80e27c9fe96c518dd31951381da6035d03fcc
MaNaS799 <manasm0609@gmail.com>
2026-01-15 10:39:18 +0530

    Update QuickSort.cpp

--- Assignment 1/QuickSort.cpp
+++ Assignment 1/QuickSort.cpp
@@ -22,34 +22,16 @@ void quickSortRec(int a[], int l, int r) {
     }
 }
 
-void quickSortIter(int a[], int n) {
-    int stack[1000];
-    int top = -1;
-    stack[++top] = 0;
-    stack[++top] = n - 1;
-    while (top >= 0) {
-        int r = stack[top--];
-        int l = stack[top--];
-        int p = partition(a, l, r);
-        if (p - 1 > l) {
-            stack[++top] = l;
-            stack[++top] = p - 1;
-        }
-        if (p + 1 < r) {
-            stack[++top] = p + 1;
-            stack[++top] = r;
-        }
-    }
-}
+
 
 int main() {
     int a[] = {10,7,8,9,1,5};
     int b[] = {10,7,8,9,1,5};
     int n = 6;
     quickSortRec(a, 0, n - 1);
-    quickSortIter(b, n);
     for (int i = 0; i < n; i++) cout << a[i] << " ";
     cout << endl;
     for (int i = 0; i < n; i++) cout << b[i] << " ";
     return 0;
 }
+
